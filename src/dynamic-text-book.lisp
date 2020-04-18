f028104e214d9bec15a6df5eea2cf7d37989edc7
David Rueda <davd33@gmail.com>
2020-04-18 20:56:08 +0200

    fix errors in dtb package

--- src/dynamic-text-book.lisp
+++ src/dynamic-text-book.lisp
@@ -12,11 +12,14 @@
 
 (defconstant action-line "* ACTION ")
 
-(defmacro next-line (var)
+(defmacro next-line (var &optional (default :none))
   "SETF VAR reading a line from *in*
 RESTART-CASES:
  - skip, returns nil"
-  `(restart-case (setf ,var (read-line *in*))
+  `(restart-case (setf ,var ,(if (eq :none default)
+                                 (print `(read-line *in*))
+                                 (print `(read-line *in* nil))))
+     (use-eof () (setf ,var :end-of-file))
      (skip () nil)
      (use-value (v) v)))
 
@@ -52,48 +55,75 @@ If REF-P is not nil, text refers to an action function."
         destination-story
         ref-p))
 
-(defun make-action (text destination-story)
+(defun make-action (text)
   "Create an action.
 An action is a bit of text that explains the transition to another story."
-  (list text
-        destination-story))
+  (list text))
+
+(defun action-text (action)
+  "Return text bit of ACTION"
+  (first action))
+
+(defun add-text-to-action (text action)
+  "Return updated ACTION with TEXT added."
+  (list (str:concat (action-text action)
+                    text)))
 
 (defun next-story (&optional story actions-p)
   "Reads *IN* and update *LINE-CURSOR* with NEXT-LINE line by line.
 If ACTIONS-P is not nil, it means that we are collecting action lines."
   (handler-bind ((end-of-file #'(lambda (c)
                                   (format t "~&WARNING: NEXT-STORY - ~A~%" c)
-                                  (invoke-restart 'skip))))
-    (let* ((line (next-line *line-cursor*)))
-      (when line
-        (let ((action-line-p (str:starts-with-p actions-line line)))
-          (if (str:starts-with-p top-level-line line)
-              ;; we reached the next section
-              story
-              ;; we continue building the current story
-              (next-story (if actions-p
-                              (add-action-button-to-story
-                               (let ((action (mapcar #'str:trim (str:split "," line))))
-                                 (multiple-value-bind (action-text quoted)
-                                     (str:replace-all "\"" "" (first action))
-                                   (make-action-button action-text
-                                                       (second action)
-                                                       (not quoted))))
-                               story)
-                              (add-text-to-story line story))
-                          action-line-p)))))))
+                                  (invoke-restart 'use-eof))))
+    (let ((line (next-line *line-cursor*)))
+      (if (not (eq :end-of-file line))
+          (let* ((action-line-p (str:starts-with-p actions-line line))
+                 (line (or (and action-line-p (next-line *line-cursor* nil))
+                           line)))
+            (if (str:starts-with-p top-level-line line)
+                ;; we reached the next section
+                story
+                ;; we continue building the current story
+                (next-story (if (and (or actions-p action-line-p)
+                                     (not (str:emptyp line)))
+                                (add-action-button-to-story
+                                 (let ((action (mapcar #'str:trim (str:split "," line))))
+                                   (multiple-value-bind (action-text quoted)
+                                       (str:replace-all "\"" "" (first action))
+                                     (make-action-button action-text
+                                                         (second action)
+                                                         (not quoted))))
+                                 story)
+                                (add-text-to-story line story))
+                            action-line-p)))
+          story))))
+
+(defun next-action (&optional action)
+  "Reads *IN* and update *LINE-CURSOR* with NEXT-LINE line by line."
+  (handler-bind ((end-of-file #'(lambda (c)
+                                  (format t "~&WARNING: NEXT-ACTION - ~A~%" c)
+                                  (invoke-restart 'use-eof))))
+    (let ((line (next-line *line-cursor*)))
+      (if (or (eq :end-of-file line)
+              (str:starts-with-p top-level-line line))
+          action
+          (next-action (add-text-to-action line action))))))
 
 (defmacro build-book (dtb-path)
   "Build the whole dynamic text book from DTB-PATH."
   (with-open-file (*in* dtb-path)
-    (let ((*line-cursor* nil))
-      (handler-bind ((end-of-file #'(lambda (c)
-                                      (format t "~&WARNING: BUILD-BOOK - ~A~%" c)
-                                      (use-value :end-of-file))))
-        (loop for l = (next-line *line-cursor*)
-           until (eq l :end-of-file)
-           collect (cond ((str:starts-with-p story-line l)
-                          `(defparameter
-                               ,(format nil "~A-story"
-                                        (second (str:split story-line l)))
-                             ',(next-story)))))))))
+    (let ((*line-cursor* (read-line *in*)))
+      (loop
+         until (eq *line-cursor* :end-of-file)
+         collect (cond ((str:starts-with-p story-line *line-cursor*)
+                        `(defparameter
+                             ,(intern
+                               (format nil "~A-story"
+                                       (second (str:split story-line *line-cursor*))))
+                           ',(next-story)))
+                       ((str:starts-with-p action-line *line-cursor*)
+                        `(defparameter
+                             ,(intern
+                               (format nil "~A-action"
+                                       (second (str:split action-line *line-cursor*))))
+                           ',(next-action))))))))
